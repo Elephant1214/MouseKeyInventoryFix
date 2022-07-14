6e4a2e1d30bfaa305e2f2cf3f10bface6350578e
Elephant1214 <nathangetsmail@gmail.com>
2022-07-14 14:12:38 -0600

    Version 1.0.0

---
+++ src/main/java/nathan/mousekeyinventoryfix/MouseKeyInventoryFix.java
@@ -0,0 +1,14 @@
+package nathan.mousekeyinventoryfix;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MouseKeyInventoryFix implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("mousekeyinventoryfix");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
