9728f4e12dde03b7e3802612b4c472942d31a1ab
Nathan <46881115+Elephant1214@users.noreply.github.com>
2022-07-15 15:22:33 -0600

    Update MouseKeyInventoryFix.java

--- src/main/java/nathan/mousekeyinventoryfix/MouseKeyInventoryFix.java
+++ src/main/java/nathan/mousekeyinventoryfix/MouseKeyInventoryFix.java
@@ -6,9 +6,4 @@ import org.slf4j.LoggerFactory;
 
 public class MouseKeyInventoryFix implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("mousekeyinventoryfix");
-
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
-    }
 }
